a0e00d8a217960b49551341f0d0e9901a0ceb0a5
Igor Kachynskyy <igor.kachynskyy@gmail.com>
2024-04-19 14:29:26 +0300

    fix: id existing for each enitties

--- src/common/Database/bases/entity.base.ts
+++ src/common/Database/bases/entity.base.ts
@@ -1,7 +1,8 @@
 import { PrimaryGeneratedColumn } from 'typeorm';
 import { ObjectBase } from './object.base';
-import { Field } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 export abstract class EntityBase<TEntity> extends ObjectBase<TEntity> {
   @Field(() => Number)
   @PrimaryGeneratedColumn({ name: 'Id' })
